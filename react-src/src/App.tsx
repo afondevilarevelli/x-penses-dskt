1219a50a1643e6ea74c65bd7e04a0f929233571c
afondevilarevelli <antonny.42@gmail.com>
2023-10-08 22:02:06 -0300

    Layout

--- react-src/src/App.tsx
+++ react-src/src/App.tsx
@@ -1,43 +1,30 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { filesystem } from '@neutralinojs/lib';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { Button } from '@mui/material';
+import React, { useEffect } from "react";
+import "./styles/global.css";
+import { filesystem } from "@neutralinojs/lib";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Routes from "./Routes";
+import Layout from "./layout/Layout";
 
 function App() {
-
   // Log current directory or error after component is mounted
   useEffect(() => {
-    filesystem.readDirectory('./').then((data) => {
-      console.log(data)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }, [])
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn asd
-        </a>
-      </header>
+    filesystem
+      .readDirectory("./")
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-      <Button variant="contained">Hello world</Button>
-    </div>
+  return (
+    <Layout>
+      <Routes />
+    </Layout>
   );
 }
 
